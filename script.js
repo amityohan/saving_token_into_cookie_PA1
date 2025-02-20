9026a036fba60f7f64d5590bd6e7383475441a32
amityohan <amit.yohan@kalvium.community>
2025-02-20 11:31:28 +0530

    finished the tasks

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+const jwt =require('jsonwebtoken');
+const secretKey='secret'
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
 }
 
 const decrypt = (token) => {
   // return decoded payload
+  try{
+    return jwt.verify(token, secretKey);
+  }catch(er){
+    console.log('Decryption failed:', er.message)
+    return null;
+  }
 }
 
 module.exports = {
   encrypt,
   decrypt
 }
+
